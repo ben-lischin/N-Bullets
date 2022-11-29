2658ddd8c3c9dd62d266504fde9997227a0d1638
Ben Lischin <94412143+ben-lischin@users.noreply.github.com>
2022-11-28 22:12:17 -0500

    Delete PlayGame.java

--- PlayGame.java
+++
@@ -1,8 +0,0 @@
-class PlayGame {
-  PlayGame() {}
-  public static void main(String[] args) {
-    ILoGamePiece bruh = new MtLoGamePiece();
-    MyGame realWord = new MyGame(1, bruh, 0, 0);
-    realWord.bigBang(500, 300, 1.0 / 28.0);
-  }
-}
